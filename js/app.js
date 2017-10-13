4e708f9415050772c224ba4f68404ed69fba5d17
Niels Christian <me@nielschristian.dk>
2017-10-13 16:16:42 +0200

    first

---
+++ js/app.js
@@ -0,0 +1,56 @@
+//import * as jsonFile from 'http://localhost/dotasjov/data2.json';
+var dataURL = './data.json';
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    heroes: null,
+    matchups: null,
+    selected: [],
+    advantages: {},
+    adv_sorted: []
+  },
+
+  created: function() {
+    this.fetchData();
+  },
+  watch: {
+    selected: function() {
+      for (adv_hero in this.heroes){
+        var result = 100;
+        for (n in this.selected){
+          var sel_hero = this.selected[n]
+          result += this.matchups[adv_hero][sel_hero];
+        };
+        this.advantages[adv_hero]['adv'] = result;
+      };
+      this.adv_sorted = _.orderBy(this.advantages, ['adv','name'], ['desc','asc'])
+    }
+  },
+  methods: {
+    fetchData: function () {
+      this.$http.get( dataURL).then(response => {
+        this.heroes = response.body.heroes;
+        this.matchups = response.body.matchups;
+        for (hero in this.heroes) {
+          this.advantages[hero] = { hero: hero, name: this.heroes[hero][0], adv: (Math.random()*10).toFixed(0)};
+        };
+      })
+    },
+
+    toggleSelect: function(key) {
+      if (!this.selected.includes(key) && this.selected.length < 5) {
+        this.selected.push(key);
+      }
+      else if (this.selected.includes(key)) {
+        var index = this.selected.indexOf(key);
+        this.selected.splice(index, 1);
+      }
+      return;
+    },
+    imgSrc(hero, db=0){
+      if (db) { return this.heroes[hero][1] }
+      else {return "./img/" + hero + ".jpg"}
+    }
+  }
+});
